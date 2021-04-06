8e219041c3ceccdd4784b419d37b0320d3f0a03e
raihan1995 <raihanahmed786@gmail.com>
2021-04-06 18:16:20 +0000

    testing

---
+++ service2/tests/test_app.py
@@ -0,0 +1,19 @@
+from unittest.mock import patch
+from flask_testing import TestCase
+from flask import url_for
+from app import app
+
+class TestBase(TestCase):
+    def create_app(self):
+        return app
+
+class TestResponse(TestBase):
+
+    def test_letters(self):
+        response = self.client.get(url_for('letters'))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_get_letters(self):
+        response = self.client.get(url_for('letters'))
+        self.assertIsNotNone(b'',response.data)
+    
